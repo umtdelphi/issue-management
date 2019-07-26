cd2dac55c5fbe7b4fcc3ff5655e50d02f1f8dab5
ukarakas <umtdelphi@gmail.com>
2019-07-26 12:29:36 +0300

    first project

---
+++ src/main/java/com/umtk/issuemanagement/util/ApiPaths.java
@@ -0,0 +1,13 @@
+package com.umtk.issuemanagement.util;
+
+public final class ApiPaths {
+    private static final String Base_Path = "/api";
+
+    public static final class IssurCtrl{
+        public static final String Ctrl_ = Base_Path +"/issue";
+    }
+
+    public static final class ProjectCtrl {
+        public static final String Ctrl_ =Base_Path +"/project";
+    }
+}
